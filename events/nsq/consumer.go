7d4078e5d83636ffb9eb2faff110e2a628073911
Yudha Pratama Putra <yudha.pratama@salestock.id>
2020-06-20 14:07:40 +0700

    Update: add start & graceful stop for consumer

--- events/nsq/consumer.go
+++ events/nsq/consumer.go
@@ -14,26 +14,36 @@ var (
 )
 
 type NsqEventConsumer struct {
+	nsq           *nsq.Config
+	consumer      *nsq.Consumer
 	Config        *config.Config
 	Topic         string
 	Channel       string
 	HandleMessage func(message *nsq.Message) error
 }
 
-func (e *NsqEventConsumer) Consume() error {
-	config := nsq.NewConfig()
-	consumer, err := nsq.NewConsumer(e.Topic, e.Channel, config)
+func (e *NsqEventConsumer) Start() {
+	log.Println("Starting nsq consumer : topic %v - channel %v", e.Topic, e.Channel)
+	e.nsq = nsq.NewConfig()
+	e.consumer, err := nsq.NewConsumer(e.Topic, e.Channel, e.nsq)
 	if err != nil {
-		log.Printf("err create consumer : %v \n", err)
-		return err
+		log.Fatalf("error creating consumer %v", err.Error())
+		return
 	}
+}
 
-	consumer.AddHandler(nsq.HandlerFunc(e.HandleMessage))
-	err = consumer.ConnectToNSQD(fmt.Sprintf("%s:%s", e.Config.NsqHost, e.Config.NsqPort))
+func (e *NsqEventConsumer) Consume() error {
+	e.consumer.AddHandler(nsq.HandlerFunc(e.HandleMessage))
+	err = e.consumer.ConnectToNSQD(fmt.Sprintf("%s:%s", e.Config.NsqHost, e.Config.NsqPort))
 	if err != nil {
 		log.Printf("err connect to nsqd : %v \n", err)
 		return err
 	}
 	waitGroup.Wait()
 	return nil
+}
+
+func (e *NsqEventConsumer) Stop() error {
+	log.Println("stoping consumer...")
+	e.consumer.Stop()
 }
