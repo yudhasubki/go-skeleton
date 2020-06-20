b8298de1c27951a6b6f1b60ecc6b1bb93e8f989e
Yudha Pratama Putra <yudha.pratama@salestock.id>
2020-06-20 16:13:29 +0700

    Update: remove health func in http layer

--- domain/example/handler.go
+++ domain/example/handler.go
@@ -1,14 +1,5 @@
 package example
 
-import (
-	"log"
-	"net/http"
-)
-
 type Handler struct {
 	Service *Service `inject:"service"`
-}
-
-func (h *Handler) Health(w http.ResponseWriter, r *http.Request) {
-	log.Println("Server is alive.")
 }
